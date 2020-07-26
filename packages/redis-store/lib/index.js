7622958e02c8405c681b400ba5fb9675b4bae4da
Glenn Hinks <ghinks@yahoo.com>
2020-07-26 11:42:27 -0400

    feat: https fix linting issues

--- packages/redis-store/lib/index.js
+++ packages/redis-store/lib/index.js
@@ -1,14 +1,14 @@
-// const path = require("path");
-// const fs = require("fs");
+const path = require("path");
+const fs = require("fs");
 const debug = require("debug")("session");
 const express = require("express");
 const session = require("express-session");
 const RedisStore = require("connect-redis")(session);
 const redis = require("redis");
 const isDocker = require("is-docker");
-// const https = require("https");
+const https = require("https");
 
-// const fsp = fs.promises;
+const fsp = fs.promises;
 const app = express();
 const secret = "keyboardcat";
 const REDIS_PORT = 6379;
